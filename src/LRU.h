8677ebe7896f2ad4aa3d3aa59d743fce488a0c62
André Ijiri Ribeiro <andre.ijiri.ribeiro@gmail.com>
2026-06-26 11:43:10 -0300

    feat(LRU): Configurado o TAD LRU

---
+++ src/LRU.h
@@ -0,0 +1,32 @@
+#ifndef _LRU_H
+#define _LRU_H
+
+#include "priorityQueue.h"
+
+
+
+
+
+/** MODELO: LRU (Least Recently Used) - Algoritmo de Substituição de Página
+ * Este modelo implementa o algoritmo de substituição de página LRU (Least Recently Used), 
+ * que substitui a página menos recentemente utilizada quando ocorre uma falta de página.
+ * 
+ * ELe faz isso mantendo uma lista de prioridade das páginas em memória, 
+ * onde a página mais recentemente utilizada é movida para o final da lista.
+ */
+
+
+
+
+
+/*                                                  FUNÇÕES PRINCIPAIS                                                  */
+/*######################################################################################################################*/
+
+
+
+
+
+/*                                                  FUNÇÕES AUXILIARES                                                  */
+/*######################################################################################################################*/
+
+#endif // _LRU_H
