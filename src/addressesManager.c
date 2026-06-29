43bf92d8aea80233f6c9321ec32f7924236a789b
CaioSwe <caio.sweiver.carvalho@uel.br>
2026-06-29 07:06:19 -0300

    feat(addressesManager): Finalizada a implementação do addresses manager, dado que memoryManager já converte os bits de offset e frame.

--- src/addressesManager.c
+++ src/addressesManager.c
@@ -1,5 +1,8 @@
 #include "addressesManager.h"
 
+#include <string.h>
+#include <stdio.h>
+
 #include "memoryManager.h"
 
 /**
@@ -15,51 +18,62 @@ void processAddrFile(MemoryManager memMng, const char* fPathAddr, FILE* fSaida){
     FILE* addrFile = fopen(fPathAddr, "r");
     // 1.1: verificar se o arquivo foi aberto corretamente
     if(addrFile == NULL){
-        printf("SUA MENSAGEM DE ERRO AQUI");
+        printf("[ERROR] Error opening addresses file.\n");
         return;
     }
 
     // 2: Processar endereços do addresses.txt usando memMng
     // TO-DO
     char line[256];
-    int virtualAddress;
-    int physicalAddress;
-    char content;
+    int virtualAddress = 0;
+    int lin = 0;
+
+    int hit = 0;
+    int miss = 0;
+    int fault = 0;
     while(fgets(line, sizeof(line), addrFile) != NULL){
         // 2.1: Lê a linha do arquivo
-        sscanf(line, "%s", virtualAddress);
+        sscanf(line, "%d", &virtualAddress);
 
         // Verifica se a linha é um comando especial (PageTable ou TLB) ou se é um simples endereço virtual. 
         // Se for um comando especial, chama a função correspondente do gerenciador de memória. 
         if(strcmp(line, "PageTable") == 0){
-            memoryManager_printPageTable(memMng);
+            memoryManager_printPageTable(memMng, fSaida);
         }else if(strcmp(line, "TLB") == 0){
-            memoryManager_printTLB(memMng);
+            memoryManager_printTLB(memMng, fSaida);
         }
         // Caso contrário, processa o endereço virtual normalmente.
         else{
-            // 2.2: Converte a linha lida em um endereço virtual (int)
-            virtualAddress = atoi(line);
+            Info pInfo = memoryManager_accessAddress(memMng, virtualAddress);
+            memoryManager_printAddressInfo(memMng, pInfo, virtualAddress, fSaida);
 
-            // 2.3: Separa o endereço virtual em número da página e offset
-            int pageNumber = virtualAddress / 100;  // Ex: 16916 / 100 = 169
-            int offset     = virtualAddress % 100;  // Ex: 16916 % 100 = 16
-
-            // 2.4: Obter o número do frame correspondente ao número da página usando memMng
-            int frameNumber = memoryManager_getFrameNumber(memMng, pageNumber);
-
-            // 2.5: Transformar o número do frame e o offset em endereço físico
-            physicalAddress = (frameNumber * 100) + offset; // Ex: (20 * 100) + 16 = 2016
-
-            // 2.6: ler conteúdo do endereço físico
-            content = memoryManager_getPageContentAt(memMng, physicalAddress);
-
-            // 2.7: escrever em fSaida
-            // Template de output:
-            // Endereço Virtual: 16916  Endereço Físico: 20 Conteúdo: '?'
-            fprintf(fSaida, "Endereço Virtual: %d  Endereço Físico: %d Conteúdo: '%c'\n", virtualAddress, physicalAddress, content);
+            AccessResult result = memoryManager_getInfoResult(pInfo);
+        
+            switch(result){
+                case ACCESS_TLB_HIT:
+                    hit += 1;
+                    break;
+                case ACCESS_PAGE_TABLE_HIT:
+                    miss += 1;
+                    break;
+                case ACCESS_PAGE_FAULT:
+                    fault += 1;
+                    break;
+            default:
+                break;
+            }
         }
     }
+
+    ////////////////////////////
+    // Estatisticas
+    ////////////////////////////
+
+    fprintf(fSaida, "\n == STATISTICS OF EXECUTION == ");
+    fprintf(fSaida, "\n[%d] TLB hits", hit);
+    fprintf(fSaida, "\n[%d] TLB misses", miss);
+    fprintf(fSaida, "\n[%d] Page faults", fault);
+    fprintf(fSaida, "\n ============================= ");
 
     fclose(addrFile);
 }
