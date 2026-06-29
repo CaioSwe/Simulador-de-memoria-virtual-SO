89c23fb90402cbaaf924698f7d31c1ae7ddc3c53
CaioSwe <caio.sweiver.carvalho@uel.br>
2026-06-29 07:00:24 -0300

    feat(memoryManager): Finalizado implementação do gerenciador de memória.

--- src/memoryManager.c
+++ src/memoryManager.c
@@ -1,46 +1,59 @@
 // Implementa o FIFO ou LRU
 #include "memoryManager.h"
 #include "priorityQueue.h"
-#include "lista.h"
 
 #include "queue.h"
 
 #include <stdlib.h>
+#include <math.h>
 
 // Estruturas do gerenciador de memoria
 ////////////////////////////////////////
 
-typedef struct InfoStr{
-    int page;
-    int frame;
+// Para cada elemento da tabela de páginas / TLB
+typedef struct PageInfoStr{
+    int pageNumber;
+    int frameNumber;
     bool ValidatingBit;
+} PageInfoStr;
+
+// Retorno da consulta retorna um objeto desta struct
+typedef struct InfoStr{
+    PageInfoStr information;
+    AccessResult aResult;
 } InfoStr;
 
+// Estrutura da TLB
 typedef struct TLBStr{
     PageReplacementAlgorithm fPageReplacementAlg;
-    printFunc pFunc;
     runThroughItems runFunc;
-    freeFunc fFunc;
+    highFreeFunc fFunc;
     
     Structure dataStructure;
 } TLBStr;
 
+// Estrutura da tabela de páginas
 typedef struct PageTableStr{
     PageReplacementAlgorithm fPageReplacementAlg;
-    printFunc pFunc;
     runThroughItems runFunc;
-    freeFunc fFunc;
+    highFreeFunc fFunc;
 
     Structure dataStructure;
+    
+    PageInfoStr* entries;
 } PageTableStr;
 
+// Estrutura do gerenciador de memória
 typedef struct memoryManagerStr{
     FILE* binFile;
     
+    int currentAmountOfFrames;
     int frameCount;
     int frameSize;
 
-    Lista physicalMemory;
+    char* physicalMemory;
+
+    printFunc pFunc;
 
     PageTableStr* pageTable;
     TLBStr* TLB;
@@ -49,7 +62,7 @@ typedef struct memoryManagerStr{
 ////////////////////////////////////////
 
 // Inicializa uma memoryMng, com arquivo .bin para memória, quantQuadros e uma estrutura de TLB (se for nula, cria um memoryManager sem TLB)
-MemoryManager memoryManager_Init(const char* binPath, int frameCount, int frameSize){
+MemoryManager memoryManager_Init(const char* binPath, int frameCount, int frameSize, printFunc pFunc){
     memoryManagerStr* mMng = (memoryManagerStr*)malloc(sizeof(memoryManagerStr));
 
     if(checkAllocation(mMng, "msg de erro")) return NULL;
@@ -64,47 +77,54 @@ MemoryManager memoryManager_Init(const char* binPath, int frameCount, int frameS
 
     mMng->binFile = binFile;
 
-    mMng->physicalMemory = criaLista();
+    mMng->physicalMemory = (char*)calloc(frameCount * frameSize, sizeof(char));
 
+    mMng->currentAmountOfFrames = 0;
     mMng->frameCount = frameCount;
     mMng->frameSize = frameSize;
 
-    // // Ler binPath para popular a lista (physicalMemory)
-    // Lista lista = mMng->physicalMemory;
-    // while(true){
-    //     inserirFim(lista, ); // ? ! a b
-    // }
+    mMng->pFunc = pFunc;
 
     mMng->pageTable = NULL;
     mMng->TLB = NULL;
 
-    fclose(binFile);
     return mMng;
 }
 
-static TLBStr* memoryManager_InitPageTable(PageReplacementAlgorithm fPageReplacementAlg, Structure pageTableStructure){
+static PageTableStr* memoryManager_InitPageTable(int size, PageReplacementAlgorithm fPageReplacementAlg, Structure pageTableStructure, runThroughItems runFunc, highFreeFunc fFunc){
     PageTableStr* pTable = (PageTableStr*)malloc(sizeof(PageTableStr));
 
     if(checkAllocation(pTable, "msg de erro")) return NULL;
 
     pTable->fPageReplacementAlg = fPageReplacementAlg;
     pTable->dataStructure = pageTableStructure;
+    pTable->runFunc = runFunc;
+    pTable->fFunc = fFunc;
+    pTable->entries = (PageInfoStr*)malloc(sizeof(PageInfoStr) * size);
+
+    for(int i = 0; i < size; i++){
+        pTable->entries[i].pageNumber = i;
+        pTable->entries[i].ValidatingBit = 0;
+        pTable->entries[i].frameNumber = -1;
+    }
 
     return pTable;
 }
 
-static TLBStr* memoryManager_InitTLB(PageReplacementAlgorithm fPageReplacementAlg, Structure tlbStructure){
+static TLBStr* memoryManager_InitTLB(PageReplacementAlgorithm fPageReplacementAlg, Structure tlbStructure, runThroughItems runFunc, highFreeFunc fFunc){
     TLBStr* tlb = (TLBStr*)malloc(sizeof(TLBStr));
 
     if(checkAllocation(tlb, "msg de erro")) return NULL;
 
     tlb->fPageReplacementAlg = fPageReplacementAlg;
     tlb->dataStructure = tlbStructure;
+    tlb->runFunc = runFunc;
+    tlb->fFunc = fFunc;
 
     return tlb;
 }
 
-void memoryManager_addPageTable(MemoryManager memMng, PageReplacementAlgorithm fPageReplacementAlg, Structure pageTableStructure){
+void memoryManager_addPageTable(MemoryManager memMng, int size, PageReplacementAlgorithm fPageReplacementAlg, Structure pageTableStructure, runThroughItems runFunc, highFreeFunc fFunc){
     memoryManagerStr* mMng = (memoryManagerStr*)memMng;
 
     if(mMng == NULL){
@@ -112,10 +132,10 @@ void memoryManager_addPageTable(MemoryManager memMng, PageReplacementAlgorithm f
         return;
     }
 
-    mMng->pageTable = memoryManager_InitPageTable(fPageReplacementAlg, pageTableStructure);
+    mMng->pageTable = memoryManager_InitPageTable(size, fPageReplacementAlg, pageTableStructure, runFunc, fFunc);
 }
 
-void memoryManager_addTLB(MemoryManager memMng, PageReplacementAlgorithm fPageReplacementAlg, Structure tlbStructure){
+void memoryManager_addTLB(MemoryManager memMng, PageReplacementAlgorithm fPageReplacementAlg, Structure tlbStructure, runThroughItems runFunc, highFreeFunc fFunc){
     memoryManagerStr* mMng = (memoryManagerStr*)memMng;
 
     if(mMng == NULL){
@@ -123,100 +143,229 @@ void memoryManager_addTLB(MemoryManager memMng, PageReplacementAlgorithm fPageRe
         return;
     }
 
-    mMng->TLB = memoryManager_InitTLB(fPageReplacementAlg, tlbStructure);
+    mMng->TLB = memoryManager_InitTLB(fPageReplacementAlg, tlbStructure, runFunc, fFunc);
 }
 
-void memoryManager_printPageTable(MemoryManager memMng){
+static void memoryManager_printAddressTableInfo(Info item, FILE* fOutput){
+    PageInfoStr pInfo = ((InfoStr*)item)->information;
 
+    fprintf(fOutput, "%d - %d - %d\n", pInfo.pageNumber, pInfo.frameNumber, pInfo.ValidatingBit);
 }
 
-// Retorna o estado da TLB (se houve inicialização)
-void memoryManager_printTLB(MemoryManager memMng){
+static void memoryManager_printAddressTableInfoVoid(Info item, void* extra){
+    memoryManager_printAddressTableInfo(item, (FILE*)extra);
+}
+
+void memoryManager_printPageTable(MemoryManager memMng, FILE* fOutput){
+    memoryManagerStr* mMng = (memoryManagerStr*)memMng;
+
+    runThroughItems runFunc = mMng->pageTable->runFunc;
+
+    runFunc(mMng->pageTable->dataStructure, memoryManager_printAddressTableInfoVoid, fOutput);
+}
+
+void memoryManager_printTLB(MemoryManager memMng, FILE* fOutput){
     memoryManagerStr* mMng = (memoryManagerStr*)memMng;
 
     runThroughItems runFunc = mMng->TLB->runFunc;
-    printFunc pFunc = mMng->TLB->pFunc;
 
-    // pFunc();
+    runFunc(mMng->TLB->dataStructure, memoryManager_printAddressTableInfoVoid, fOutput);
 }
 
-///////////////
-/*
-    3) Incluir dois comandos no arquivo address.txt:
-        a) Comando PageTable: escreve a tabela de páginas da seguinte forma:
-        ###########
-        Página - Quadro - Bit Validade [e outras informações que vc armazenou na tabela de páginas]
-        .... uma linha para cada página do processo ...
-        ###########
-        b) Comando TLB: escreve todas as entradas da TLB
-        ************
-        Página - Quadro - Bit Validade [e outras informações que vc armazenou na tabela de páginas]
-        [ uma linha para cada página da TLB ]
-        ************
-*/
-
-void memoryManager_printAddressInfo(MemoryManager memMng, FILE* fOutput, Info item){
+void memoryManager_printAddressInfo(MemoryManager memMng, Info item, int address, FILE* fOutput){
     memoryManagerStr* mMng = (memoryManagerStr*)memMng;
-    InfoStr* info = (InfoStr*)item;
+    PageInfoStr pInfo = ((InfoStr*)item)->information;
+    
+    printFunc pFunc = mMng->pFunc;
+    int frameNumber = pInfo.frameNumber;
+    
+    int offset = address & (mMng->frameSize - 1);
+    
+    char pageContent = mMng->physicalMemory[(frameNumber * mMng->frameSize) + offset];
 
-    fprintf(fOutput, "%d - %d - %d\n", info->page, info->frame, info->ValidatingBit);
+    int physicalAddress = (pInfo.frameNumber * mMng->frameSize) + offset;
+    const char* value = pFunc(&pageContent, NULL);
+
+    fprintf(fOutput, "Endereço virtual: %d | Endereço físico: %d | Conteúdo: %s\n", address, physicalAddress, value);
 }
 
 bool memoryManager_isInTLB(MemoryManager memMng, int address){
     memoryManagerStr* mMng = (memoryManagerStr*)memMng;
 
-    PageReplacementAlgorithm repTLB = mMng->TLB->fPageReplacementAlg;
-    Structure tlbStructure = mMng->TLB->dataStructure;
-
-    return repTLB(tlbStructure, address, false, NULL);
-}
-
-// Move o endereço da tabela de páginas para a TLB manualmente
-void memoryManager_moveAddressIntoTLB(MemoryManager memMng, int address){
-    memoryManagerStr* mMng = (memoryManagerStr*)memMng;
+    if(mMng->TLB == NULL) return false;
 
     PageReplacementAlgorithm repTLB = mMng->TLB->fPageReplacementAlg;
     Structure tlbStructure = mMng->TLB->dataStructure;
 
-    repTLB(tlbStructure, address, true, NULL);
+    PageInfoStr* info = (PageInfoStr*)malloc(sizeof(PageInfoStr));
+    int pageNumber = address >> (int)log2(mMng->frameSize);
+
+    info->pageNumber = pageNumber;
+
+    bool isInTlb = repTLB(tlbStructure, info, false, NULL);
+
+    free(info);
+
+    return isInTlb;
 }
 
-void memoryManager_moveAddressIntoPageTable(MemoryManager memMng, int address){
+static void memoryManager_insertPageInMemory(memoryManagerStr* mMng, int pageNumber, int frameNumber){
+    int physicalFrameId = frameNumber * mMng->frameSize;
+
+    fseek(mMng->binFile, pageNumber * mMng->frameSize, SEEK_SET);
+    fread(mMng->physicalMemory + physicalFrameId, mMng->frameSize, 1, mMng->binFile);
+}
+
+Info memoryManager_accessAddress(MemoryManager memMng, int address){
     memoryManagerStr* mMng = (memoryManagerStr*)memMng;
 
-    PageReplacementAlgorithm repPageTable = mMng->pageTable->fPageReplacementAlg;
-    Structure pTableStructure = mMng->pageTable->dataStructure;
+    if(mMng == NULL) return NULL;
 
-    repPageTable(pTableStructure, address, true, NULL);
+    InfoStr* info = (InfoStr*)malloc(sizeof(InfoStr));
+
+    int pageNumber = address >> (int)log2(mMng->frameSize);
+
+    info->information.pageNumber = pageNumber;
+
+    bool hasTLB = (mMng->TLB != NULL);
+
+    // Consulta TLB
+    if(hasTLB){
+        PageReplacementAlgorithm repTLB = mMng->TLB->fPageReplacementAlg;
+        Structure tlbStructure = mMng->TLB->dataStructure;
+        
+        if(repTLB(tlbStructure, &info->information, false, NULL)){
+            repTLB(tlbStructure, &info->information, true, NULL); // Promove
+
+            info->aResult = ACCESS_TLB_HIT;
+            info->information.frameNumber = mMng->pageTable->entries[pageNumber].frameNumber;
+            info->information.ValidatingBit = 1;
+            return info;
+        }
+    }
+
+    // Se não foi um TLB hit, consulta a tabela de páginas
+    PageReplacementAlgorithm repPT = mMng->pageTable->fPageReplacementAlg;
+    Structure ptStructure = mMng->pageTable->dataStructure;
+
+    PageInfoStr entrada = mMng->pageTable->entries[pageNumber];
+
+    bool isInMemory = entrada.ValidatingBit;
+
+    // Esta na memoria
+    if(isInMemory){
+        info->aResult = ACCESS_PAGE_TABLE_HIT;
+        
+        info->information.frameNumber = entrada.frameNumber;
+        info->information.ValidatingBit = 1;
+
+        return info;
+    }
+
+    // Nao esta na memoria
+    int frameNumber = -1;
+
+    // Memoria nao esta cheia
+    if(mMng->currentAmountOfFrames < mMng->frameCount){
+        frameNumber = mMng->currentAmountOfFrames;
+
+        info->information.frameNumber = frameNumber;
+        info->information.ValidatingBit = 1;
+
+        repPT(ptStructure, &info->information, true, NULL); // Insere no algoritmo
+        
+        mMng->currentAmountOfFrames += 1;
+    }
+    // Memoria esta cheia
+    else{
+        Info removedInfo = NULL;
+        repPT(ptStructure, &info->information, true, &removedInfo); // Substitui
+
+        frameNumber = ((PageInfoStr*)removedInfo)->frameNumber;
+        printf(" -> %d", frameNumber);
+        int removedPage = ((PageInfoStr*)removedInfo)->pageNumber;
+
+        mMng->pageTable->entries[removedPage].frameNumber = -1;
+        mMng->pageTable->entries[removedPage].ValidatingBit = 0;
+    }
+
+    memoryManager_insertPageInMemory(mMng, pageNumber, frameNumber);
+
+    mMng->pageTable->entries[pageNumber].frameNumber = frameNumber;
+    mMng->pageTable->entries[pageNumber].ValidatingBit = 1;
+
+    if(hasTLB){
+        PageReplacementAlgorithm repTLB = mMng->TLB->fPageReplacementAlg;
+        Structure tlbStructure = mMng->TLB->dataStructure;
+
+        repTLB(tlbStructure, &info->information, true, NULL); // Insere na TLB
+    }
+
+    info->aResult = ACCESS_PAGE_FAULT;
+    return info;
 }
 
-PageContent memoryManager_getPageContentAt(MemoryManager memMng, int address){
+int memoryManager_getFrameNumber(Info info){
+    PageInfoStr* pInfo = (PageInfoStr*)info;
 
+    return pInfo->frameNumber;
 }
 
-int memoryManager_getFrameNumber(MemoryManager memmng, int address){
+AccessResult memoryManager_getInfoResult(Info info){
+    InfoStr* pInfo = (InfoStr*)info;
 
+    return pInfo->aResult;
 }
 
-void memoryManager_free(MemoryManager memMng){
+void memoryManager_setPageNumber(Info info, int pageNumber){
+    PageInfoStr* pInfo = (PageInfoStr*)info;
+
+    pInfo->pageNumber = pageNumber;
+}
+
+void memoryManager_setFrameNumber(Info info, int frameNumber){
+    PageInfoStr* pInfo = (PageInfoStr*)info;
+
+    pInfo->frameNumber = frameNumber;
+}
+
+bool memoryManager_comparePagesInfo(Info info1, Info info2){
+    PageInfoStr* i1 = (PageInfoStr*)info1;
+    PageInfoStr* i2 = (PageInfoStr*)info2;
+    
+    return i1->pageNumber == i2->pageNumber;
+}
+
+static void memoryManager_freePageInfo(Info pInfo, void* extra){
+    PageInfoStr* pInfoStr = (PageInfoStr*)pInfo;
+
+    free(pInfoStr);
+}
+
+static void memoryManager_freePageTable(PageTableStr* pageTable, void* extra){
+    if(pageTable == NULL) return;
+
+    pageTable->fFunc(pageTable->dataStructure, memoryManager_freePageInfo, extra);
+    free(pageTable->entries);
+    free(pageTable);
+}
+
+static void memoryManager_freeTLB(TLBStr* tlb, void* extra){
+    if(tlb == NULL) return;
+
+    tlb->fFunc(tlb->dataStructure, memoryManager_freePageInfo, extra);
+    free(tlb);
+}
+
+void memoryManager_free(MemoryManager memMng, void* extra){
     memoryManagerStr* mMng = (memoryManagerStr*)memMng;
 
     fclose(mMng->binFile);
     
-    memoryManager_freePageTable(mMng->pageTable);
-    memoryManager_freeTLB(mMng->TLB);
+    memoryManager_freePageTable(mMng->pageTable, extra);
+    memoryManager_freeTLB(mMng->TLB, extra);
 
-    limparLista(mMng->physicalMemory, NULL, NULL);
+    free(mMng->physicalMemory);
 
     free(mMng);
-}
-
-static void memoryManager_freePageTable(PageTableStr* pageTable){
-    pageTable->fFunc(runExtra, pageTable->dataStructure);
-    free(pageTable);
-}
-
-static void memoryManager_freeTLB(TLBStr* tlb){
-    tlb->fFunc(runExtra, tlb->dataStructure);
-    free(tlb);
 }
--- src/memoryManager.h
+++ src/memoryManager.h
@@ -2,6 +2,9 @@
 #define _MEMORYMANAGER_H_
 
 #include <stdbool.h>
+#include <stdio.h>
+
+#include "types.h"
 
 /**
  * Cabecalho especializado para a implementacao de um gerenciador de memoria virtual.
@@ -13,10 +16,19 @@ typedef void* MemoryManager;
 
 typedef void* Structure;
 typedef void* Info;
-typedef const char* PageContent;
+typedef char PageContent;
 
 // Declaracao de escopo padrao para um algoritmo de troca de paginas.
-typedef int (*PageReplacementAlgorithm) (Structure tableStructure, int addressToInsert, bool replace, void* extra);
+typedef bool (*PageReplacementAlgorithm) (Structure tableStructure, Info pageInfo, bool replace, void* extra);
+
+/////////////////////////////////////////////////////////////////////
+
+// Resultado de uma consulta (accessAddress)
+typedef enum {
+    ACCESS_TLB_HIT,
+    ACCESS_PAGE_TABLE_HIT,
+    ACCESS_PAGE_FAULT
+} AccessResult;
 
 /////////////////////////////////////////////////////////////////////
 
@@ -25,44 +37,59 @@ typedef int (*PageReplacementAlgorithm) (Structure tableStructure, int addressTo
  * @param binPath O caminho de arquivo do .bin para memoria auxiliar (Backing Store).
  * @param frameCount A quantidade de frames disponiveis na memoria fisica simulada.
  * @param frameSize O tamanho de cada quadro/pagina em bytes.
+ * @param pFunc Um ponteiro para a função de impressão.
  * @return Retorna um ponteiro para o gerenciador de memoria criado.
  */
-MemoryManager memoryManager_Init(const char* binPath, int frameCount, int frameSize);
+MemoryManager memoryManager_Init(const char* binPath, int frameCount, int frameSize, printFunc pFunc);
 
 /**
  * @brief Vincula uma estrutura de tabela de paginas ao gerenciador de memoria.
  * @param memMng O objeto gerenciador de memoria a ser modificado.
+ * @param size Tamanho da tabela de paginas.
  * @param fPageReplacementAlg Funcao do algoritmo de substituicao de paginas para a tabela.
  * @param pageTableStructure A estrutura de dados escolhida para representar a tabela.
+ * @param runFunc Um ponteiro para a funcao de percorrimento da estrutura associada.
+ * @param fFunc Um ponteiro para uma funcao de liberacao da estrutura.
  * @return Nao ha retorno de algum valor.
  */
-void memoryManager_addPageTable(MemoryManager memMng, PageReplacementAlgorithm fPageReplacementAlg, Structure pageTableStructure);
+void memoryManager_addPageTable(MemoryManager memMng, int size, PageReplacementAlgorithm fPageReplacementAlg, Structure pageTableStructure, runThroughItems runFunc, highFreeFunc fFunc);
 
 /**
  * @brief Vincula uma estrutura de cache TLB ao gerenciador de memoria.
  * @param memMng O objeto gerenciador de memoria a ser modificado.
  * @param fPageReplacementAlg Funcao do algoritmo de substituicao de entradas na TLB.
  * @param tlbStructure A estrutura de dados escolhida para representar a TLB.
+ * @param runFunc Um ponteiro para a funcao de percorrimento da estrutura associada.
+ * @param fFunc Um ponteiro para uma funcao de liberacao da estrutura.
  * @return Nao ha retorno de algum valor.
  */
-void memoryManager_addTLB(MemoryManager memMng, PageReplacementAlgorithm fPageReplacementAlg, Structure tlbStructure);
+void memoryManager_addTLB(MemoryManager memMng, PageReplacementAlgorithm fPageReplacementAlg, Structure tlbStructure, runThroughItems runFunc, highFreeFunc fFunc);
 
 /**
- * @brief Exibe no console o estado de ocupacao e os mapeamentos presentes na tabela de paginas.
+ * @brief Exibe o estado de ocupacao e os mapeamentos presentes na tabela de paginas.
  * @param memMng O objeto gerenciador de memoria de referencia.
+ * @param fOutput Um ponteiro para o arquivo a ser escrito.
  * @return Nao ha retorno de algum valor.
  */
-void memoryManager_printPageTable(MemoryManager memMng);
+void memoryManager_printPageTable(MemoryManager memMng, FILE* fOutput);
 
 /**
- * @brief Exibe no console o estado de ocupacao e os mapeamentos presentes na TLB.
+ * @brief Exibe o estado de ocupacao e os mapeamentos presentes na TLB.
  * @param memMng O objeto gerenciador de memoria de referencia.
+ * @param fOutput Um ponteiro para o arquivo a ser escrito.
  * @return Nao ha retorno de algum valor.
  */
-void memoryManager_printTLB(MemoryManager memMng);
+void memoryManager_printTLB(MemoryManager memMng, FILE* fOutput);
 
-// ...
-void memoryManager_printAddressInfo(MemoryManager memMng, FILE* fOutput, Info item);
+/**
+ * @brief Exibe as informacoes associadas a informacao passada de uma consulta.
+ * @param memMng O objeto gerenciador de memoria de referencia.
+ * @param item A informação associada ao memoryManager a ser impressa no arquivo.
+ * @param address Endereco a ser impresso (utilizado para offset).
+ * @param fOutput Um ponteiro para o arquivo a ser escrito.
+ * @return Nao ha retorno de algum valor.
+ */
+void memoryManager_printAddressInfo(MemoryManager memMng, Info item, int address, FILE* fOutput);
 
 /**
  * @brief Verifica se o mapeamento de um determinado endereco logico esta no cache TLB.
@@ -73,43 +100,57 @@ void memoryManager_printAddressInfo(MemoryManager memMng, FILE* fOutput, Info it
 bool memoryManager_isInTLB(MemoryManager memMng, int address);
 
 /**
- * @brief Move manualmente o mapeamento de um endereco consultado para dentro da TLB.
- * @param memMng O objeto gerenciador de memoria a ser modificado.
- * @param address O endereco logico que sera espelhado no cache.
- * @return Nao ha retorno de algum valor.
- */
-void memoryManager_moveAddressIntoTLB(MemoryManager memMng, int address);
-
-/**
- * @brief Move manualmente o mapeamento de um endereco do disco para a tabela de paginas.
- * @param memMng O objeto gerenciador de memoria a ser modificado.
- * @param address O endereco logico carregado do arquivo auxiliar apos um Page Fault.
- * @return Nao ha retorno de algum valor.
- */
-void memoryManager_moveAddressIntoPageTable(MemoryManager memMng, int address);
-
-/**
- * @brief Obtem o numero do frame fisico associado a um determinado endereco logico.
+ * @brief Obtem a informação de uma página na memória.
  * @param memMng O objeto gerenciador de memoria de referencia.
- * @param address O endereco logico cuja correspondencia fisica sera buscada.
+ * @param address Endereço virtual correspondente ao acesso.
+ * @return Retorna um ponteiro de informação relacionada à consulta.
+ */
+Info memoryManager_accessAddress(MemoryManager memMng, int address);
+
+/**
+ * @brief Obtem o numero do frame fisico associado a uma consulta feita.
+ * @param info Informação de uma consulta à memória usando accessAddress.
  * @return Retorna o indice numerico do frame correspondente na memoria fisica.
  */
-int memoryManager_getFrameNumber(MemoryManager memMng, int address);
+int memoryManager_getFrameNumber(Info info);
 
 /**
- * @brief Traduz o endereco logico e retorna o ponteiro para o conteudo de dados da pagina.
- * @note Esta funcao encapsula a validacao automatica na TLB e a copia do binario em caso de Page Fault.
- * @param memMng O objeto gerenciador de memoria ativo.
- * @param address O endereco logico cujo conteudo textual sera lido.
- * @return Retorna um ponteiro para o array de caracteres que representa os dados da pagina.
+ * @brief Obtem o resultado de uma consulta feita. Podendo ser HIT, MISS ou FAULT.
+ * @param info Informação de uma consulta à memória usando accessAddress.
+ * @return Retorna o resultado da consulta.
  */
-PageContent memoryManager_getPageContentAt(MemoryManager memMng, int address);
+AccessResult memoryManager_getInfoResult(Info info);
+
+/**
+ * @brief Seta o numero de pagina (virtual) associado a uma consulta.
+ * @param info Informação de uma consulta à memória usando accessAddress.
+ * @param pageNumber Numero da pagina a ser associada.
+ * @return Nao ha' retorno de algum valor.
+ */
+void memoryManager_setPageNumber(Info info, int pageNumber);
+
+/**
+ * @brief Seta o numero do frame fisico associado a uma consulta.
+ * @param info Informação de uma consulta à memória usando accessAddress.
+ * @param frameNumber Numero do frame a ser associado.
+ * @return Nao ha' retorno de algum valor.
+ */
+void memoryManager_setFrameNumber(Info info, int frameNumber);
+
+/**
+ * @brief Compara duas informacoes de pa'ginas.
+ * @param info1 Ponteiro para uma informacao de pa'gina.
+ * @param info2 Ponteiro para uma informacao de pa'gina.
+ * @return Retorna verdadeiro caso as pa'ginas sejam verdadeiras, falso caso contra'rio.
+ */
+bool memoryManager_comparePagesInfo(Info info1, Info info2);
 
 /**
  * @brief Fecha os arquivos de suporte e libera a memoria alocada para o gerenciador.
  * @param memMng O objeto gerenciador de memoria a ser desalocado.
+ * @param extra Ponteiro extra para algum conteúdo adicional
  * @return Nao ha retorno de algum valor.
  */
-void memoryManager_free(MemoryManager memMng);
+void memoryManager_free(MemoryManager memMng, void* extra);
 
 #endif
